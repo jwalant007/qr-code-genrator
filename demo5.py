d0c03eefd7768b22cfceec7d9ad628b2088dcc7f
Jwalant <jwalantjbhatt@gmail.com>
2025-06-03 17:06:41 +0530

    Updated Flask app for redeployment on Render

--- demo5.py
+++ demo5.py
@@ -6,7 +6,7 @@ from waitress import serve
 from io import BytesIO
 
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST", "localhost"),
+    "host": os.getenv("DB_HOST", "127.0.0.1"),
     "user": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD", "jwalant"),
     "database": os.getenv("DB_NAME", "listdb"),
