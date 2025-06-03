13ac57ffca43815d60a4c28029cee7c158d1b520
Jwalant <jwalantjbhatt@gmail.com>
2025-06-03 16:09:10 +0530

    Updated Flask app for redeployment on Render

--- demo5.py
+++ demo5.py
@@ -37,7 +37,7 @@ def create_app():
     @app.route("/generate_qr/<name>")
     def generate_qr(name):
         """Generate a QR code dynamically"""
-        qr_url = f"https://127.0.0.1/students/{name}"
+        qr_url = f"https://qr-code-genrator-xpcv.onrender.com/students/{name}"
         print(f"Generating QR for: {qr_url}")  
 
         qr = qrcode.QRCode(version=1, box_size=10, border=5)
