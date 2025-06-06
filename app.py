a96300c43eb0ef6e74572a0ac675ee09a3c46da9
Jwalant <jwalantjbhatt@gmail.com>
2025-06-06 16:08:11 +0530

    Updated Flask app for redeployment on Render

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "127.0.0.1"),
     "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "jwalant"),
+    "password": os.getenv("DB_PASSWORD", ""),
     "database": os.getenv("DB_NAME", "listdb"),
     "port": int(os.getenv("DB_PORT", 3306))
 }
