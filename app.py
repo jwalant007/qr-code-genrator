bf0b008f6e57d61e6ba75090bba16e7fda766e6c
Jwalant <jwalantjbhatt@gmail.com>
2025-06-06 16:09:30 +0530

    Updated Flask app for redeployment on Render

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
 
     logging.info(f"🚀 Running Flask app on port {port} with Waitress")
-    serve(app, host="0.0.0.0", port=port)
+    serve(app, host="127.0.0.1", port=port)
