f38738f4565f8119bfb74f8a7c8d66a9f527e1cd
Jwalant <jwalantjbhatt@gmail.com>
2025-06-09 23:38:07 +0530

    Updated Flask app for redeployment on Render

--- app.py
+++ app.py
@@ -77,18 +77,18 @@ def create_app():
 
     @app.route("/generate_qr/<name>")
     def generate_qr(name):
-      
+      ''''
         qr_url = f"https://qr-code-genrator-xpcv.onrender.com/student/{name}"
         qr = qrcode.QRCode(version=1, box_size=10, border=5)
         qr.add_data(qr_url)
         qr.make(fit=True)
-        img = qr.make_image(fill_color="black", back_color="white")
+        img = qr.make_image(fill_color="black", back_color="white")'''
 
-        qr_io = BytesIO()
-        img.save(qr_io, format="PNG")
-        qr_io.seek(0)
+    qr_io = BytesIO()
+    qr_url = f"https://qr-code-genrator-xpcv.onrender.com/student/{name}"
+    qr_io.seek(0)
 
-        return send_file(qr_io, mimetype="image/png")
+    return send_file(qr_io, mimetype="image/png")
 
     @app.route("/student/<name>")
     def display_student(name):
